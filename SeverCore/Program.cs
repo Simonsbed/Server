0eb44ddfee5f05cae025d66096f5c954826c0ac5
MetoZec <87604547+Simonsbed@users.noreply.github.com>
2024-12-11 21:41:51 +0900

    컴파일러 최적화

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -5,41 +5,27 @@ namespace SeverCore
 {
 	class Program
 	{
-		static void MainThread(object state)
+		volatile static bool _stop = false;
+		static void ThreadMain()
 		{
-			for (int i = 0; i < 5; i++)
+			Console.WriteLine("쓰레드 시작!");
+			while (_stop == false)
 			{
-				Console.WriteLine("Hello Thread!");
+				// 누군가가 stop 신호를 해주기를 기다린다
 			}
-		}
+            Console.WriteLine("쓰레드 종료");
+        }
 		static void Main(string[] args)
 		{
+			Task t = new Task(ThreadMain); t.Start();
+			Thread.Sleep(1000);
 
-			ThreadPool.SetMinThreads(1, 1);
-			ThreadPool.SetMaxThreads(5, 5);
+			_stop = true;
 
-			for (int i = 0; i < 5; i++)
-			{
-				Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-				t.Start();
-			}
-
-			//for (int i = 0; i < 4; i++)
-			//{
-			//	ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-			//}
-
-			ThreadPool.QueueUserWorkItem(MainThread);
-
-			//Thread t = new Thread(MainThread); 
-			//t.Name = "Test";
-			//t.IsBackground = false;
-			//t.Start();
-
-			//t.Join();
-			//Console.WriteLine("Hello World");
-
-			while (true) { }
+            Console.WriteLine("Stop 호출");
+			Console.WriteLine("종료 대기중");
+			t.Wait();
+			Console.WriteLine("종료 성공");
 		}
 	}
 }
