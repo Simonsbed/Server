df1e41d913178ed77df9dc7f9c4c5e674d882d29
MetoZec <87604547+Simonsbed@users.noreply.github.com>
2025-01-02 20:51:16 +0900

    ReadWriteLock

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -4,59 +4,39 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SeverCore
 {
-	//class Lock
-	//{
-	//	// bool <- 커널
-	//	ManualResetEvent _available = new ManualResetEvent(false);
-	//	//AutoResetEvent _available = new AutoResetEvent(true);
-	//	public void Acquire()
-	//	{
-	//		_available.WaitOne(); // 입장 시도
-	//		_available.Reset(); // 문을 닫느다 
-	//	}
-
-	//	public void Release() 
-	//	{
-	//		_available.Set();
-	//	}
-	//}
-	 
+	
 	class Program
 	{
-		static int _num = 0;
-		static Mutex _lock = new Mutex();
-		static void Thread_1() 
-		{
-			for (int i = 0; i < 10000; i++)
-			{
-				_lock.WaitOne();
-				_num--;
-				_lock.ReleaseMutex();
-			}
-		
-		}
-
-		static void Thread_2()
-		{
-			for (int i = 0; i < 10000; i++)
-			{
-				_lock.WaitOne();
-				_num++;
-				_lock.ReleaseMutex();
-			}
-		}
+		static volatile int count = 0;
+		static Lock _lock = new Lock();
 
 		static void Main(string[] args)
 		{
-			Task t1 = new Task(Thread_1);
-			Task t2 = new Task(Thread_2);
+			Task t1 = new Task(delegate ()
+			{
+				for (int i = 0; i < 100000; i++)
+				{
+					_lock.WriteLock();
+					count++;
+					_lock.WriteUnLock();
+				}
+			});
+			Task t2 = new Task(delegate ()
+			{
+				for (int i = 0; i < 100000; i++)
+				{
+					_lock.WriteLock();
+					count--;
+					_lock.WriteUnLock();
+				}
+			});
+
 			t1.Start();
 			t2.Start();
 
 			Task.WaitAll(t1, t2);
 
-			Console.WriteLine(_num);
-
+			Console.WriteLine(count);
 		}
 	}
 }
